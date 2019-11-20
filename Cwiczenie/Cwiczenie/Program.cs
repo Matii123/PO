4dfb41459de9d0ffb55708086ed025996b39630e
Matii123 <46341879+Matii123@users.noreply.github.com>
2019-11-20 15:47:10 +0100

    Add files via upload

---
+++ Cwiczenie/Cwiczenie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pracownik p = new programista();
+            p.Pracuj();
+
+            Console.ReadKey();
+        }
+    }
+}
